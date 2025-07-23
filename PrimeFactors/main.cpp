8c7fe1067f141edf8fd99c18ef1d2ef64e095e38
yunzhen-kim <kyj886811@gmail.com>
2025-07-23 10:07:29 +0900

    refactor generatePrimeFactors
    Signed-off-by: yunzhen-kim <kyj886811@gmail.com>

--- PrimeFactors/main.cpp
+++ PrimeFactors/main.cpp
@@ -39,6 +39,36 @@ TEST_F(PrimeFactorsFixture, Of6) {
 	EXPECT_EQ(expected, pf.generatePrimeFactors(6));
 }
 
+TEST_F(PrimeFactorsFixture, Of7) {
+	expected = {7};
+	EXPECT_EQ(expected, pf.generatePrimeFactors(7));
+}
+
+TEST_F(PrimeFactorsFixture, Of8) {
+	expected = {2,2,2};
+	EXPECT_EQ(expected, pf.generatePrimeFactors(8));
+}
+
+TEST_F(PrimeFactorsFixture, Of9) {
+	expected = {3,3};
+	EXPECT_EQ(expected, pf.generatePrimeFactors(9));
+}
+
+TEST_F(PrimeFactorsFixture, Of10) {
+	expected = { 2,5 };
+	EXPECT_EQ(expected, pf.generatePrimeFactors(10));
+}
+
+TEST_F(PrimeFactorsFixture, Of11) {
+	expected = { 11 };
+	EXPECT_EQ(expected, pf.generatePrimeFactors(11));
+}
+
+TEST_F(PrimeFactorsFixture, Of12) {
+	expected = {2,2,3};
+	EXPECT_EQ(expected, pf.generatePrimeFactors(12));
+}
+
 int main() {
 	InitGoogleMock();
 	return RUN_ALL_TESTS();
--- PrimeFactors/prime_factors.cpp
+++ PrimeFactors/prime_factors.cpp
@@ -6,16 +6,13 @@ class PrimeFactors {
 public:
 	vector<int> generatePrimeFactors(int number) {
 		vector<int> result = {};
-		if (number == 1) return result;
-		if (number > 1) {
-			int devisor = 2;
-			for (devisor = 2; number > 1; ++devisor) {
-				while (number % devisor == 0) {
-					result.push_back(devisor);
-					number /= devisor;
-				}
+		int devisor = 2;
+		for (devisor = 2; number > 1; ++devisor) {
+			while (number % devisor == 0) {
+				result.push_back(devisor);
+				number /= devisor;
 			}
-			return result;
 		}
+		return result;
 	}
 };
